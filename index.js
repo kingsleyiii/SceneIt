8ccb58b0e1e68683a9d1f61598d4477c4ec8ad0c
Kingsley Reeves III <kingsleyiii@Kingsleys-MBP.home>
2022-08-30 20:41:05 -0400

    Added a render movies function and styled with CSS

---
+++ index.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // code here will execute after the document is loaded
+    });
+function renderMovies(movieArray) {
+    let movieHtmlArray = movieArray.map(currentMovie => {
+       return `<div>
+       <h2>${currentMovie.Title}</h2>`
+    })
+    return movieHtmlArray.join(``)
+
+}

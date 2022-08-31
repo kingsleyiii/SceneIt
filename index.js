e5bcda44b7cda7b72db76295010e75c00ebec82e
Kingsley Reeves III <kingsleyiii@Kingsleys-MBP.home>
2022-08-30 21:23:23 -0400

    Added some functionality to the submit button

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // code here will execute after the document is loaded
-    });
+const movieContainer = document.getElementById("movies-container")
+const myForm = document.getElementById("search-form")
+
+
+
 function renderMovies(movieArray) {
+    // console.log("hello")
     let movieHtmlArray = movieArray.map(currentMovie => {
-       return `<div>
-       <h2>${currentMovie.Title}</h2>`
+        return `<div>
+        <h2>${currentMovie.Title}</h2>
+        </div>`
     })
     return movieHtmlArray.join(``)
-
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    myForm.addEventListener("submit", e => {
+        e.preventDefault();
+        console.log("hello")
+        // code here will execute after the document is loaded
+        movieContainer.innerHTML = renderMovies(movieData)
+        });
+
+})
+
+

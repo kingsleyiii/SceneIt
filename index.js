4ed0b8dd6c84946a162997401989d1f164e1bfbd
Kingsley Reeves III <kingsleyiii@Kingsleys-MBP.home>
2022-09-01 17:29:59 -0400

    Made a watchlist which saves the movies

--- index.js
+++ index.js
@@ -6,8 +6,11 @@ const myForm = document.getElementById("search-form")
 function renderMovies(movieArray) {
     // console.log("hello")
     let movieHtmlArray = movieArray.map(currentMovie => {
-        return `<div>
+        return `<img class="card-img-top" src=${currentMovie.Poster} />
+        <div class="movie card-body">
         <h2>${currentMovie.Title}</h2>
+        <p>${currentMovie.Year}</p>
+        <button class="add-button" data-imdbid="${currentMovie.imdbID}">Add Movie</button>
         </div>`
     })
     return movieHtmlArray.join(``)
@@ -16,11 +19,33 @@ function renderMovies(movieArray) {
 document.addEventListener('DOMContentLoaded', function() {
     myForm.addEventListener("submit", e => {
         e.preventDefault();
-        console.log("hello")
         // code here will execute after the document is loaded
         movieContainer.innerHTML = renderMovies(movieData)
         });
 
 })
 
+document.addEventListener("click", function(e) {
+    if (e.target.classList.contains("add-button")) {
+        movieID = e.target.dataset.imdbid
+        saveToWatchlist(movieID)
+        // console.log(movieID)
+    }
+})
+
+function saveToWatchlist(movieID) {
+    console.log(movieID)
+    const movie = movieData.find(function(currentMovie){
+        return currentMovie.imdbID == movieID
+    })
+    let watchlistJSON = localStorage.getItem('watchlist');
+    let watchlist = JSON.parse(watchlistJSON);
+    if (watchlist == null) {
+        watchlist = [];
+    }
+    watchlist.push(movie);
+    watchlistJSON = JSON.stringify(watchlist);
+    localStorage.setItem('watchlist', watchlistJSON);
+    console.log(watchlist);
+}
 
